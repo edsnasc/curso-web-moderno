2a746f32071464a7805cd5e62e10c97bce15f428
edsnasc <edsnascimento89@gmail.com>
2020-11-11 15:21:52 -0300

    add capitulo react

---
+++ Excercicios-Web/react/exercicios/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
+
+ReactDom.render(
+<div>
+    <Pai nome="Paulo" sobrenome="Silva">
+     {/* Como passo os componentes  Filhos aqui */}
+     <Filho nome="Pedro" />
+     <Filho nome="Paulo" />
+     <Filho nome="Carla" />
+    </ Pai>
+</div>
+,  document.getElementById('root'))
